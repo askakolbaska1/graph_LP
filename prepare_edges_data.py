a6a6b1b870c5914908a120b6f82387fe5d756376
askakolbaska1 <ivanche_nerv@mail.ru>
2026-02-24 17:30:57 +0300

    first att to decode

--- prepare_edges_data.py
+++ prepare_edges_data.py
@@ -1,18 +1,14 @@
 import pandas as pd
-import glob
 
-files = glob.glob("data/edges/raw/*.csv")
-dfs = []
-for f in files:
-    df = pd.read_csv(f)
-    dfs.append(df)
+main_data = pd.read_csv("data/edges/triples.csv")
 
-main_data = pd.concat(dfs, ignore_index=True)
+main_data = main_data.drop_duplicates()
+main_data = main_data[main_data['id_entity_1'] != main_data['id_entity_2']]
 
-main_data = main_data.drop(['name_entity_1', 'sequence_entity_1', 'name_entity_2', 'sequence_entity_2'], axis=1)
-main_data['relation'] = 'interacts_with'
+del_entity = [72756, 72757, 63113]
 
-main_data = main_data[main_data['id_entity_1'] != 63113]
-main_data = main_data[main_data['id_entity_2'] != 63113]
+for i in del_entity:
+    main_data = main_data[main_data['id_entity_1'] != i]
+    main_data = main_data[main_data['id_entity_2'] != i]
 
-main_data.to_csv("data/edges/IW_edges.csv", index=False)
+main_data.to_csv("data/edges/triples.csv", index=False)
